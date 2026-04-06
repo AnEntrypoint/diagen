7301b6ba6e96acf380027430c516c0180b22188b
lanmower <almagestfraternite@gmail.com>
2026-04-07 00:21:17 +0200

    Set wasmPaths for WebGPU ORT JSEP backend to fix worker crash

--- gh-pages-src/demo/app.js
+++ gh-pages-src/demo/app.js
@@ -1,4 +1,4 @@
-const worker = new Worker('./worker.js?v=51', { type: 'module' })
+const worker = new Worker('./worker.js?v=52', { type: 'module' })
 const ttsWorker = new Worker('./tts-worker.js', { type: 'module' })
 const SpeechRecognition = window.SpeechRecognition ?? window.webkitSpeechRecognition
 const synth = window.speechSynthesis
